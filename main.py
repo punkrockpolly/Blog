fffb8ea8bc81e999a3ba94d65dd7b52141041677
Polina Giralt <polina.giralt@gmail.com>
2013-10-22 14:45:55 -0400

    add ascii art page handling and template

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import jinja2
 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), 
+                               autoescape=True)
 
 
 def render_str(template, **params):
@@ -39,14 +39,20 @@ class WelcomePage(Handler):
 
 
 class Blog(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now_add = True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now_add=True)
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", new_post = self)
+        return render_str("post.html", new_post=self)
+
+
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
 
 
 class MainPage(Handler):
@@ -64,7 +70,7 @@ class NewPost(Handler):
         content = self.request.get("content")
 
         if subject and content:
-            new_post = Blog(subject = subject, content = content)
+            new_post = Blog(subject=subject, content=content)
             new_post.put()
             new_post_id = new_post.key().id()
             self.redirect('/blog/%s' % new_post_id)
@@ -86,9 +92,31 @@ class PostPage(Handler):
         self.render("postpage.html", display_post=display_post)
 
 
+class AsciiPage(Handler):
+    def render_front(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+        self.render("ascii_front.html", title=title, art=art, error=error, arts=arts)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title=title, art=art)
+            a.put()
+            self.redirect("/ascii")
+
+        else:
+            error = "we need both a title and some artwork!"
+            self.render_front(title, art, error)
+
+
 app = webapp2.WSGIApplication([('/', WelcomePage),
                                ('/blog', MainPage),
                                ('/blog/newpost', NewPost),
                                ('/blog/(\d+)', PostPage),
-                               ],
-                               debug=True)
+                               ('/ascii', AsciiPage),
+                               ], debug=True)
